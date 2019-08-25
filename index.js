a1cae58a6d1083438cd841bb1969f94cfd05b390
Aurea <aurea.soares@engenhaira.ufjf.br>
2019-08-25 20:43:07 -0300

    tentativa de comunicacao com API

---
+++ index.js
@@ -0,0 +1,26 @@
+const API_URL = "https://api.sympla.com.br/public/v3/events";
+
+const proxyurl = "http://127.0.0.1:5500/"; //https://cors-anywhere.herokuapp.com/";
+
+fetch(API_URL, {
+  method: 'GET',
+  mode: 'cors',
+  headers: {
+    "s_token": "d0750f54e88d664128e4f19b7b6369418c9258c538c319029bda6e659bf260fc",
+    "Host": "api.sympla.com.br"
+    }
+})
+  .then(response => response.text())
+  .then(result => {
+    // const data = JSON.parse(result)
+    console.log(result);
+  });
+
+// let xhr = new XMLHttpRequest();
+// xhr.open("GET", "https://api.sympla.com.br/public/v3/events")
+// xhr.setRequestHeader('s_token', 'd0750f54e88d664128e4f19b7b6369418c9258c538c319029bda6e659bf260fc');
+// // xhr.setRequestHeader('Access-Control-Allow-Origin', 'disable');
+// // xhr.setRequestHeader('Access-Control-Allow-Headers', 'Content-Type');
+// xhr.withCredentials = true;
+
+// xhr.send();
