00e2672a0d0607948e43a61b1795334d6b9b5ac4
Aurea <aurea.soares@engenhaira.ufjf.br>
2019-08-26 14:52:02 -0300

    inicializaçao api maps

--- index.js
+++ index.js
@@ -2,19 +2,19 @@ const API_URL = "https://api.sympla.com.br/public/v3/events";
 
 const proxyurl = "http://127.0.0.1:5500/"; //https://cors-anywhere.herokuapp.com/";
 
-fetch(API_URL, {
-  method: 'GET',
-  mode: 'cors',
-  headers: {
-    "s_token": "d0750f54e88d664128e4f19b7b6369418c9258c538c319029bda6e659bf260fc",
-    "Host": "api.sympla.com.br"
-    }
-})
-  .then(response => response.text())
-  .then(result => {
-    // const data = JSON.parse(result)
-    console.log(result);
-  });
+// fetch(API_URL, {
+//   method: 'GET',
+//   mode: 'cors',
+//   headers: {
+//     "s_token": "d0750f54e88d664128e4f19b7b6369418c9258c538c319029bda6e659bf260fc",
+//     "Host": "api.sympla.com.br"
+//     }
+// })
+//   .then(response => response.text())
+//   .then(result => {
+//     // const data = JSON.parse(result)
+//     console.log(result);
+//   });
 
 // let xhr = new XMLHttpRequest();
 // xhr.open("GET", "https://api.sympla.com.br/public/v3/events")
